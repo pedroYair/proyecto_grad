3e3e9de35844a4dfc200cb17730dcc22cae7d958
pedro <pcaravall17@gmail.com>
2018-03-31 20:12:04 -0500

    Commit 93: Agregando lista de estudios entrevista

--- tecnicas/apps/entrevista/urls.py
+++ tecnicas/apps/entrevista/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url
 from django.contrib.auth.decorators import login_required
-from .views import crearEstudio
+from .views import CrearEstudio, ListaEstudios
 
 # nombre de la url, view que respondera y el parametro name
 urlpatterns = [
 
     # Urls modelo Estudio_entrevista
-    url(r'^agregar_estudio/$', crearEstudio.as_view(), name='estudio'),
+    url(r'^agregar_estudio/$', CrearEstudio.as_view(), name='estudio'),
+    url(r'^lista_estudios/$', ListaEstudios.as_view(), name='lista_estudios'),
 
 ]
--- tecnicas/apps/entrevista/views.py
+++ tecnicas/apps/entrevista/views.py
@@ -2,12 +2,22 @@ from django.shortcuts import render
 from .models import *
 from .forms import FormEstudio
 from django.core.urlresolvers import reverse_lazy, reverse
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView
+
+"""------------------------------------VIEWS MODELO ESTUDIOENTREVISTA------------------------------------"""
 
 
-class crearEstudio(CreateView):
+class CrearEstudio(CreateView):
     model = EstudioEntrevista
     form_class = FormEstudio
     template_name = 'estudio/crear_estudio_entrevista.html'
-    success_url = reverse_lazy('entrevista:estudio')
+    success_url = reverse_lazy('entrevista:lista_estudios')
+
+
+class ListaEstudios(ListView):
+    model = EstudioEntrevista
+    template_name = 'estudio/lista_estudios_entrevista.html'
+    context_object_name = 'estudios'
+    paginate_by = 20
+
 
